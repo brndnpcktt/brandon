2d2c7be80baa008b31deb9ee280745f7b1e04be9
brndnpcktt <brndnpcktt1@gmail.com>
2013-09-23 03:29:55 -0400

    everything to date
    3:29 AM, 9-23-13

---
+++ js/scrollin.js
@@ -0,0 +1,3 @@
+var scrollnum=0;
+function increaseScrollNavi(scrollnum) {return ++scrollnum; window.location.hash=scrollnum;}
+function decreaseScrollNavi(scrollnum) {return --scrollnum; window.location.hash=scrollnum;}
